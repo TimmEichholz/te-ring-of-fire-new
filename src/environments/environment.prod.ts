61072aa183ec758c77ebef2cb2a311ed402368bc
TimmEichholz <karate3ule@hotmail.de>
2022-05-09 10:10:06 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-new',
+    appId: '1:270259875168:web:51c764e0231bf8c8128205',
+    storageBucket: 'ring-of-fire-new.appspot.com',
+    apiKey: 'AIzaSyBFki9nPT6qvaF9jwJhHdyLg70I1i-BnuM',
+    authDomain: 'ring-of-fire-new.firebaseapp.com',
+    messagingSenderId: '270259875168',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-new',
+    appId: '1:270259875168:web:51c764e0231bf8c8128205',
+    storageBucket: 'ring-of-fire-new.appspot.com',
+    apiKey: 'AIzaSyBFki9nPT6qvaF9jwJhHdyLg70I1i-BnuM',
+    authDomain: 'ring-of-fire-new.firebaseapp.com',
+    messagingSenderId: '270259875168',
+  },
   production: false
 };
 
